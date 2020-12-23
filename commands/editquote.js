7d4d1772fef46c0d806fbf4b01a2fb1d42ebe8bd
Nicholas Christopher <curiousnicklee@gmail.com>
2020-12-22 16:17:44 -0800

    Editing quotes - v1.2.0
    - Added .editquote command, based on the .newquote command.
    - Tweaked some wording in .newquote

--- commands/editquote.js
+++ commands/editquote.js
@@ -23,50 +23,45 @@ const mentionParse = (mention, client) => {
 module.exports = {
 	enabled: true,
 	hidden: false,
-	name: "newquote",
-	description:
-		"Creates a new quote",
-	usage: "[quote] [author]",
-	example: "You're gonna have a bad time Sans",
-	aliases: ["createquote", "addquote", "nquote"],
+	name: "editquote",
+	description: "Edits the specified quote",
+	usage: "[id] [quote] [author]",
+	example: "",
+	aliases: ["equote", "quoteedit"],
 	cooldown: 10,
 	args: true,
 	guildOnly: true,
 	supportGuildOnly: false,
 	execute(message, args) {
-		if (
-			db.get(`${message.guild.id}.allquote`) ||
-			message.member.hasPermission("MANAGE_GUILD")
-		) {
-			if (args.length < 2) {
+		if (message.member.hasPermission("MANAGE_GUILD")) {
+			if (args.length < 3) {
 				const invalidArgsEmbed = new Discord.MessageEmbed()
 					.setTitle("❌ Incorrect usage")
 					.setColor(config.colors.error)
 					.setDescription(
-						"You have to specify the quote's text & the author."
+						"You have to specify an ID, text, and the author."
 					);
 				return message.channel.send(invalidArgsEmbed);
 			}
 
+			const quoteID = args.shift();
+
 			const serverQuotes = db.get(`${message.guild.id}.quotes`) || [];
-			if (
-				serverQuotes.length >=
-				(db.get(`${message.guild.id}.maxQuotes`) || 30)
-			) {
-				const fullQuotesEmbed = new Discord.MessageEmbed()
-					.setTitle("❌ Storage full")
+			const quote = serverQuotes[quoteID - 1];
+
+			if (!quote) {
+				const quoteNotFoundEmbed = new Discord.MessageEmbed()
+					.setTitle("❌ No quote found")
 					.setColor(config.colors.error)
-					.setDescription(
-						`This server has too many quotes! Remove some with \`${message.applicablePrefix}deletequote\` before adding more.`
-					);
-				return message.channel.send(fullQuotesEmbed);
+					.setDescription("Couldn't find a quote with that ID.");
+				return message.channel.send(quoteNotFoundEmbed);
 			}
 
-			const author = mentionParse(args.pop(), message.client);
-			const quote = args.join(" ");
+			const editedAuthor = mentionParse(args.pop(), message.client);
+			const editedText = args.join(" ");
 
 			if (
-				quote.length >
+				editedText.length >
 				(db.get(`${message.guild.id}.maxQuoteSize`) || 130)
 			) {
 				const quoteSizeEmbed = new Discord.MessageEmbed()
@@ -80,31 +75,29 @@ module.exports = {
 				return message.channel.send(quoteSizeEmbed);
 			}
 
-			db.push(`${message.guild.id}.quotes`, {
-				text: quote,
-				author: author,
-				createdTimestamp: Date.now(),
-				quoter: message.author.id,
-			});
+			serverQuotes[quoteID - 1] = {
+				text: editedText,
+				author: editedAuthor,
+				createdTimestamp: quote.createdTimestamp,
+				editedTimestamp: Date.now(),
+				quoter: quote.quoter,
+				editor: message.author.id,
+			};
+
+			db.set(`${message.guild.id}.quotes`, serverQuotes);
 
 			const successEmbed = new Discord.MessageEmbed()
-				.setTitle("✅ Added quote")
+				.setTitle("✅ Edited quote")
 				.setColor(config.colors.success)
-				.setDescription(
-					`Created a new server quote:
-						
-						"${quote}" - ${author}`
-				)
-				.setFooter(`Quote #${(serverQuotes.length || 0) + 1}`);
+				.setDescription(`"${editedText}" - ${editedAuthor}`)
+				.setFooter(`Quote #${quoteID}`);
 			return message.channel.send(successEmbed);
 		} else {
 			const noPermissionEmbed = new Discord.MessageEmbed()
 				.setTitle("❌ You don't have permission to do that")
 				.setColor(config.colors.error)
 				.setDescription(
-					`That action requires the Manage Guild permission.
-					
-					**❗ To allow anyone to create quotes**, have an administrator run \`${message.applicablePrefix}allquote\``
+					`That action requires the Manage Guild permission.`
 				);
 			message.channel.send(noPermissionEmbed);
 		}
