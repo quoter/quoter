4ee9a86a09863ee0e443417e07902dbe51c68694
Nicholas Christopher <nchristopher@tuta.io>
2021-03-20 19:07:04 -0700

    Apparently I forgot to add new files...

---
+++ commands/eval.js
@@ -0,0 +1,54 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+
+const config = require("../config.json");
+
+module.exports = {
+	enabled: true,
+	hidden: true,
+	name: "eval",
+	description: "Runs code from an admin",
+	usage: "",
+	example: "",
+	aliases: [],
+	cooldown: 3,
+	args: true,
+	guildOnly: false,
+	supportGuildOnly: false,
+	async execute(message, args) {
+		if (config.admins?.includes(message.author.id)) {
+			try {
+				eval(args.join(" "));
+			} catch (error) {
+				const errorEmbed = new Discord.MessageEmbed()
+					.setColor(config.colors.error)
+					.setTitle("❌ An error occurred")
+					.setDescription("I've message you more information.");
+				await message.channel.send(errorEmbed);
+
+				const detailedErrorEmbed = new Discord.MessageEmbed()
+					.setColor(config.colors.error)
+					.setTitle("❌ An error occurred")
+					.setDescription(
+						`Here's some more information:
+						\`\`\`${error}\`\`\``
+					);
+				return await message.author.send(detailedErrorEmbed);
+			}
+
+			const successEmbed = new Discord.MessageEmbed()
+				.setTitle("✅ Success")
+				.setColor(config.colors.success)
+				.setDescription(`The code ran with no errors.`);
+			await message.channel.send(successEmbed);
+		} else {
+			const noPermissionEmbed = new Discord.MessageEmbed()
+				.setTitle("❌ You don't have permission to do that")
+				.setColor(config.colors.error)
+				.setDescription(
+					`That action can only be use by administrators.`
+				);
+			await message.channel.send(noPermissionEmbed);
+		}
+	},
+};
