c7d2227ec725d0b61a2950d412c1b5c55ead38f6
Nicholas Christopher <nchristopher@tuta.io>
2021-04-10 11:04:49 -0700

    💬 Removed most embeds from .newquote

--- commands/newquote.js
+++ commands/newquote.js
@@ -53,13 +53,9 @@ module.exports = {
 				serverQuotes.length >=
 				(db.get(`${message.guild.id}.maxQuotes`) || 30)
 			) {
-				const fullQuotesEmbed = new Discord.MessageEmbed()
-					.setTitle("❌ Storage full")
-					.setColor(config.colors.error)
-					.setDescription(
-						`This server has too many quotes! Use \`${message.applicablePrefix}deletequote\` before creating more.`
-					);
-				return await message.channel.send(fullQuotesEmbed);
+				return await message.channel.send(
+					`❌ **|** This server has too many quotes! Use \`${message.applicablePrefix}deletequote\` before creating more.`
+				);
 			}
 
 			const quoteArgs = args.join(" ").split("-");
@@ -83,15 +79,11 @@ module.exports = {
 				quote.length >
 				(db.get(`${message.guild.id}.maxQuoteSize`) || 130)
 			) {
-				const quoteSizeEmbed = new Discord.MessageEmbed()
-					.setTitle("❌ Quote too big")
-					.setColor(config.colors.error)
-					.setDescription(
-						`Quotes cannot be longer than ${
-							db.get(`${message.guild.id}.maxQuoteSize`) || 130
-						} characters.`
-					);
-				return await message.channel.send(quoteSizeEmbed);
+				return await message.channel.send(
+					`❌ **|** Quotes cannot be longer than ${
+						db.get(`${message.guild.id}.maxQuoteSize`) || 130
+					} characters.`
+				);
 			}
 
 			db.push(`${message.guild.id}.quotes`, {
@@ -112,15 +104,11 @@ module.exports = {
 				.setFooter(`Quote #${(serverQuotes.length || 0) + 1}`);
 			await message.channel.send(successEmbed);
 		} else {
-			const noPermissionEmbed = new Discord.MessageEmbed()
-				.setTitle("❌ You don't have permission to do that")
-				.setColor(config.colors.error)
-				.setDescription(
-					`That action requires the Manage Guild permission.
+			await message.channel.send(
+				`✋ **|** That action requires the **Manage Guild** permission.
 
 **❗ To allow anyone to create quotes**, use \`${message.applicablePrefix}allquote\`.`
-				);
-			await message.channel.send(noPermissionEmbed);
+			);
 		}
 	},
 };
