fc3034d7efe384e85dc7c8c36f2e0244d70b75cb
Nicholas Christopher <nchristopher@tuta.io>
2021-05-29 09:30:35 -0700

    ✨ Added Guild schema

---
+++ schemas/guild.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+
+const guildSchema = new Schema({
+	_id: {
+		type: String,
+		required: true,
+	},
+	prefix: String,
+	allQuote: Boolean,
+	quotes: [
+		{
+			text: {
+				type: String,
+				required: true,
+			},
+			author: String,
+			quoterID: String,
+			ogMessageID: String,
+			ogChannelID: String,
+			createdTimestamp: {
+				type: Number,
+				min: 0,
+				default: Date.now(),
+			},
+		},
+	],
+});
+
+module.exports = model("Guild", guildSchema);
