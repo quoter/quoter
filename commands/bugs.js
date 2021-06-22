20a1fda4a48be555df137514002ec51e821d315d
Nicholas Christopher <nchristopher@tuta.io>
2021-06-22 15:22:52 -0700

    📄 Tweaked license notices
    - Added line breaks to notice.
    - Used multi-file program license notice, explicitly states that files are part of Quoter.

--- commands/bugs.js
+++ commands/bugs.js
@@ -1,10 +1,19 @@
 /*
 Copyright (C) 2020-2021 Nicholas Christopher
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, version 3.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+This file is part of Quoter.
 
-You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>
+Quoter is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, version 3.
+
+Quoter is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Quoter.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 module.exports = {
