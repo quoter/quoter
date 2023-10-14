bf30fc2974c036f648485e52320b5b18575741c2
Nick Oates <nick@nickoates.com>
2023-10-14 08:38:05 -0700

    📄 Add license header to missing files

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,3 +1,21 @@
+/*
+Copyright (C) 2020-2023 Nick Oates
+
+This file is part of Quoter.
+
+Quoter is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, version 3.
+
+Quoter is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Quoter.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import BugsCommand from "./bugs";
 import DeleteQuoteCommand from "./deletequote";
 import ExportCommand from "./export";
