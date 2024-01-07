0445d3bad11678590863b18f766021d1a0f203c4
Nick Oates <nick@nickoates.com>
2024-01-07 14:58:26 -0800

    🐛 Don't delete guilds during Discord outage

--- src/events/guildDelete.ts
+++ src/events/guildDelete.ts
@@ -20,6 +20,8 @@ import { Guild as DiscordGuild } from "discord.js";
 import { Guild } from "../schemas/guild";
 
 export default async function guildDelete(guild: DiscordGuild) {
+	if (!guild.available) return; // Server outage
+
 	await Guild.deleteOne({ _id: guild.id });
 	console.log(`Deleted data for guild ${guild.name} (${guild.id})`);
 }
