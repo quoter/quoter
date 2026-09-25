384bdeda596ad927f30686cf134165448ea550e4
Nick Oates <58091943+n1ckoates@users.noreply.github.com>
2026-09-25 15:25:44 -0700

    remove unnecessary credentials

--- apps/bot/drizzle.config.ts
+++ apps/bot/drizzle.config.ts
@@ -4,7 +4,4 @@ export default defineConfig({
 	dialect: "sqlite",
 	schema: "./src/db/schema.ts",
 	out: "./drizzle",
-	dbCredentials: {
-		url: process.env["DATABASE_PATH"] ?? "./db/quoter.sqlite",
-	},
 });
